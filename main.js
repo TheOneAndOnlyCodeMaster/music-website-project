17dd3df1fee42d46d216c851f2c6ec797bed88d6
TheOneAndOnlyCodeMaster <74931617+TheOneAndOnlyCodeMaster@users.noreply.github.com>
2022-06-13 10:40:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+song1="";
+song2="";
 function preload(){
-
+song1 = loadSound("longTime.mp3");
+song2 = loadSound("respect.mp3");
 }
 function setup(){
 canvas = createCanvas(400, 400);
 canvas.position(650, 250);
+
+video = createCapture(VIDEO);
+video.hide();
 }
 function draw(){
-
+image(video, 0, 0, 500, 400);
 }
