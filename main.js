13854d8d8899a1c059cb4e82dbf75c3b1e1aed8d
TheOneAndOnlyCodeMaster <74931617+TheOneAndOnlyCodeMaster@users.noreply.github.com>
2022-06-23 15:17:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 scorePointLeftWrist=0;
+scorePointRightWrist =0;
 song1="";
 song2="";
 leftWristX= 0;
@@ -10,8 +11,8 @@ song1 = loadSound("longTime.mp3");
 song2 = loadSound("respect.mp3");
 }
 function setup(){
-canvas = createCanvas(400, 400);
-canvas.position(650, 250);
+canvas = createCanvas(600, 600);
+canvas.position(550, 250);
 
 video = createCapture(VIDEO);
 video.hide();
@@ -20,8 +21,9 @@ posenet = ml5.poseNet(video, modelLoaded);
 posenet.on('pose', gotPoses)
 }
 function draw(){
-image(video, 0, 0, 500, 400);
+image(video, 0, 0, 700, 600);
 song1Status = song1.isPlaying();
+song2Status = song2.isPlaying();
 console.log(song1Status);
 stroke("orange");
 fill("cyan");
@@ -31,6 +33,14 @@ if(scorePointLeftWrist > 0.2){
     if(song1Status == false){
     song1.play();
     document.getElementById("currentSong").innerHTML="It's been a long time";
+    }
+}
+if(scorePointRightWrist > 0.2){
+    circle(rightWristX, rightWristY, 20);
+    song1.stop();
+    if(song2Status == false){
+    song2.play();
+    document.getElementById("currentSong").innerHTML="Respect";
     }
 }
 }
@@ -47,6 +57,7 @@ function gotPoses(results){
         console.log("left wrist x = "+leftWristX +" left wrist y = "+ leftWristY);
         console.log("right wrist x = "+rightWristX +" right wrist y = "+ rightWristY);
         scorePointLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("score point left wrist = "+scorePointLeftWrist)
+        console.log("score point left wrist = "+scorePointLeftWrist);
+        scorePointRightWrist =  results[0].pose.keypoints[10].score;
     }
 }
