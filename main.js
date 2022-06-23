8bd60b354d7c8e1655d89d1420936fb9643c3a97
TheOneAndOnlyCodeMaster <74931617+TheOneAndOnlyCodeMaster@users.noreply.github.com>
2022-06-22 22:18:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+scorePointLeftWrist=0;
 song1="";
 song2="";
 leftWristX= 0;
@@ -20,6 +21,18 @@ posenet.on('pose', gotPoses)
 }
 function draw(){
 image(video, 0, 0, 500, 400);
+song1Status = song1.isPlaying();
+console.log(song1Status);
+stroke("orange");
+fill("cyan");
+if(scorePointLeftWrist > 0.2){
+    circle(leftWristX, leftWristY, 20);
+    song2.stop();
+    if(song1Status == false){
+    song1.play();
+    document.getElementById("currentSong").innerHTML="It's been a long time";
+    }
+}
 }
 function modelLoaded(){
     console.log('posenet is ready');
@@ -33,5 +46,7 @@ function gotPoses(results){
         rightWristY = results[0].pose.rightWrist.y;
         console.log("left wrist x = "+leftWristX +" left wrist y = "+ leftWristY);
         console.log("right wrist x = "+rightWristX +" right wrist y = "+ rightWristY);
+        scorePointLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("score point left wrist = "+scorePointLeftWrist)
     }
 }
